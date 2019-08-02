fcf48ac32a8c3954a8519ccbb1387da3787768ae
yvonnekhin <yvonnekhin@hotmail.com>
2019-08-02 14:43:33 +0800

    combined with Department portal

---
+++ stationeryapp/Models/util.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace demo1.Models
+{
+    public class util
+    {
+        public static void SendEmail(string address, string subject = "", string message="")
+        {
+            string email = "sa48team2@gmail.com";
+            string password = "oobzwwosiiwtslnh";
+
+            var loginInfo = new System.Net.NetworkCredential(email, password);
+            var msg = new MailMessage();
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            msg.From = new MailAddress(email);
+            msg.To.Add(new MailAddress(address));
+            msg.Subject = subject;
+            msg.Body = message;
+            msg.IsBodyHtml = true;
+
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = loginInfo;
+            smtpClient.Send(msg);
+        }
+    }
+}
