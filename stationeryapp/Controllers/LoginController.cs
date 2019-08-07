9a9503f77590ac817b7f0a460d672ce2256ea263
lxl <lxl@DESKTOP-14I1O47>
2019-08-07 09:48:53 +0800

    Add StoreSupervisor/StoreManager controller and homepages

--- stationeryapp/Controllers/LoginController.cs
+++ stationeryapp/Controllers/LoginController.cs
@@ -22,6 +22,17 @@ namespace stationeryapp.Controllers
         {
             return View();
         }
+        //for storeMangerLogin
+        public ActionResult LoginStoreManager ()
+        {
+            return View();
+        }
+        //for storeSupervisor Login
+        public ActionResult LoginStoreSupervisor()
+        {
+            return View();
+        }
+
 
         public ActionResult EmployeeIndex(String msg)
         {
@@ -72,6 +83,7 @@ namespace stationeryapp.Controllers
                         storeclerk.SessionId = sessionId;
                         db.Entry(storeclerk).State = EntityState.Modified;
                         db.SaveChanges();
+                        ViewData["tag"] = "storeClerk";
                         return RedirectToAction("Index", "Home", new { storeclerk.SessionId });
                     }
                     else
@@ -84,9 +96,76 @@ namespace stationeryapp.Controllers
                     return View();
                 }
             }
-
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult StoreManagerLogin([Bind(Include = "UserName,Password")] StoreManager storeManager)
+        {
+            if (storeManager.UserName != null && storeManager.Password != null)
+            {
+                StoreManager storeManager1 = db1.StoreManagers.Where(p => p.UserName == storeManager.UserName).FirstOrDefault();
+                string hashedPassword = CalculateMD5Hash(storeManager.Password);
+                if (ModelState.IsValid)
+                {
+                    if (storeManager1.Password == hashedPassword)
+                    {
+                        string sessionId = Guid.NewGuid().ToString();
+                        storeManager1.Password = hashedPassword;
+                        storeManager1.SessionId = sessionId;
+                        db1.Entry(storeManager1).State = EntityState.Modified;
+                        db1.SaveChanges();
+                        ViewData["tag"] = "storeManager";
+                        return RedirectToAction("Index", "StoreManagers", new { storeManager1.SessionId, tag= "storeManager" });
+                    }
+                    else
+                    {
+                        return RedirectToAction("LoginStoreManager", "Login");
+                    }
+                }
+                else
+                {
+                    return RedirectToAction("LoginStoreManager", "Login");
+                }
+            }
+            return RedirectToAction("LoginStoreManager", "Login");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult StoreSupervisorLogin([Bind(Include = "UserName,Password")] StoreSupervisor storeSupervisor)
+        {
+            if (storeSupervisor.UserName != null && storeSupervisor.Password != null)
+            {
+                StoreSupervisor storeSupervisor1 = db1.StoreSupervisors.Where(p => p.UserName == storeSupervisor.UserName).FirstOrDefault();
+                string hashedPassword = CalculateMD5Hash(storeSupervisor.Password);
+                if (ModelState.IsValid)
+                {
+                    if (storeSupervisor1.Password == hashedPassword)
+                    {
+                        string sessionId = Guid.NewGuid().ToString();
+                        storeSupervisor1.Password = hashedPassword;
+                        storeSupervisor1.SessionId = sessionId;
+                        db1.Entry(storeSupervisor1).State = EntityState.Modified;
+                        db1.SaveChanges();
+                        return RedirectToAction("Index", "StoreSupervisors", new { storeSupervisor1.SessionId});
+                    }
+                    else
+                    {
+                        return RedirectToAction("LoginStoreSupervisor", "Login");
+                    }
+                }
+                else
+                {
+                    return RedirectToAction("LoginStoreSupervisor", "Login");
+                }
+            }
+            return RedirectToAction("LoginStoreSupervisor", "Login");
+        }
+
+
+
 
         [HttpPost]
         public ActionResult EmployeeLogin([Bind(Include = "UserName,Password")] Employee emp)
